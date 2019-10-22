cea17631bfdc877c6a42925e0def2912ee641144
Josh Coles <josh@joshcoles.com>
2019-10-22 15:58:54 -0400

    Begin building out tweet components.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "index.scss";
+import tweets from "./tweets.json";
 
-import Application from "components/Application";
+import "./styles.css";
 
-ReactDOM.render(<Application />, document.getElementById("root"));
+function Tweet(props) {
+  return(
+    <article className="tweet">
+    <header className="tweet__header">
+      <img
+        className="tweet__header-avatar"
+        src={props.avatar}
+        alt="User Avatar"
+      />
+      <h2 className="tweet__header-name">{props.name}</h2>
+    </header>
+    <main className="tweet__content">
+      <p>{props.content}</p>
+    </main>
+    <footer className="tweet__footer">{props.date}</footer>
+  </article>
+  )
+}
+
+ReactDOM.render(
+
+  tweets.map((tweet, index) => {
+    return <Tweet
+    key={tweet.id}
+    name={tweet.name}
+    avatar={tweet.avatar}
+    content={tweet.content}
+    date={tweet.date}
+  />
+  }),
+  document.getElementById("root")
+);

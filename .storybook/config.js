e954a3c7559f3364a528d6912309ce3e07229826
Josh Coles <josh@joshcoles.com>
2019-10-26 19:22:25 -0400

    Complete 'building a button' activity using storybook.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,15 @@
+// import { configure } from '@storybook/react';
+// import '../src/index.css';
+
+// const req = require.context('../src', true, /\.stories.js$/);
+
+// function loadStories() {
+//   req.keys().forEach(filename => req(filename));
+// }
+
+// configure(loadStories, module);
+
+
 import { configure } from "@storybook/react";
 
 function loadStories() {
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,19 @@
 import React from "react";
-
 import "components/Button.scss";
+import classnames from 'classnames';
 
 export default function Button(props) {
-   return <></>;
+
+   const btnClass = classnames({
+      button: true,
+      'button--confirm': props.confirm,
+      'button--danger': props.danger
+   })
+
+   return <button 
+         onClick={props.onClick} 
+         disabled={props.disabled} 
+         className={btnClass}>
+         {props.children}
+      </button>;
 }

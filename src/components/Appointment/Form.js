4151e7fbbea93f8366cf483dda7ab8e06aad9ec7
Josh Coles <josh@joshcoles.com>
2019-11-06 12:03:36 -0500

    Fix bug where onSave was not being fired at appropriate time.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -17,6 +17,10 @@ export default function Form(props) {
     props.onCancel();
   }
 
+  const save = () => {
+    props.onSave(name, interviewer)
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -37,7 +41,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button onClick={cancel} danger>Cancel</Button>
-          <Button onClick={props.onSave(name, interviewer)} confirm>Save</Button>
+          <Button onClick={save} confirm>Save</Button>
         </section>
       </section>
     </main>

b63a86564ff0e7c7a7dfd09fe4db12c3e2fdfa57
Josh Coles <josh@joshcoles.com>
2019-11-10 20:22:33 -0500

    Move schedule into app from storybook.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,55 @@
 import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from './DayList';
+import Appointment from './Appointment';
+
+const appointments = [
+
+  {
+    id: 2,
+    time: "10am",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "11am",
+    interview: {
+      student: "George Harrison",
+      interviewer: {
+        id: 1,
+        name: "Brian Epstein",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 1,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "John Lennon",
+      interviewer: {
+        id: 1,
+        name: "George Martin",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 1,
+    time: "5pm",
+  },
+];
 
 export default function Application(props) {
 
@@ -47,7 +96,11 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {
+          appointments.map((appointment) => (
+            <Appointment key={appointment.id} {...appointment} />
+          ))
+        }
       </section>
     </main>
   );
